52923d47b46021a7aac094fcee0580b7844144b1
nabsara <sarah.m.nabi@gmail.com>
2021-12-20 11:05:24 +0100

    feat: add WGAN-GP

--- src/electro_modelling/models/dcgan.py
+++ src/electro_modelling/models/dcgan.py
@@ -78,6 +78,91 @@ class DCGAN:
         else:
             raise ValueError(f"Loss {loss} not implemented in DCGAN")
 
+    def _get_gradient(self, real, fake, epsilon):
+        """
+        Return the gradient of the critic's scores with respect to mixes of real
+        and fake images.
+
+        Parameters
+        ----------
+        real :
+            current batch of real images
+        fake :
+            current batch of fake images
+        epsilon : np.array
+            vector of the uniformly random proportions of real / fake per mixed image
+        """
+        # Mix the images together
+        mixed_images = real * epsilon + fake * (1 - epsilon)
+        # Calculate the critic's scores on the mixed images
+        mixed_scores = self.discriminator(mixed_images)
+        # Take the gradient of the scores with respect to the images
+        gradient = torch.autograd.grad(
+            inputs=mixed_images,
+            outputs=mixed_scores,
+            grad_outputs=torch.ones_like(mixed_scores),
+            create_graph=True,
+            retain_graph=True,
+        )[0]
+        return gradient
+
+    def _gradient_penalty(self, gradient):
+        """
+        Return the gradient penalty given a gradient. Compute the magnitude of
+        each image's gradient for current batch gradients and penalize the mean
+        quadratic distance of each magnitude to 1.
+
+        Parameters
+        ----------
+        gradient : np.array
+            the gradient of the critic's scores with respect to the mixed image
+
+        Returns
+        -------
+            the gradient penalty
+        """
+        # Flatten the gradients so that each row captures one image
+        gradient = gradient.view(len(gradient), -1)
+        # Calculate the magnitude of every row
+        gradient_norm = gradient.norm(2, dim=1)
+        # Penalize the mean squared distance of the gradient norms from 1
+        penalty = torch.mean((gradient_norm - 1) * (gradient_norm - 1))
+        return penalty
+
+    def get_crit_loss(self, crit_fake_pred, crit_real_pred, gp, c_lambda):
+        """
+        Return the loss of a critic given the critic's scores for fake and real images,
+        the gradient penalty, and gradient penalty weight.
+        Parameters
+        ----------
+            crit_fake_pred: the critic's scores of the fake images
+            crit_real_pred: the critic's scores of the real images
+            gp: the unweighted gradient penalty
+            c_lambda: the current weight of the gradient penalty
+
+        Returns
+        -------
+            crit_loss: a scalar for the critic's loss, accounting for the relevant factors
+        """
+        crit_loss = torch.mean(crit_fake_pred - crit_real_pred + c_lambda * gp)
+        return crit_loss
+
+    def get_gen_loss(self, crit_fake_pred):
+        """
+        Return the loss of a generator given the critic's scores of the generator's fake images.
+
+        Parameters
+        ----------
+        crit_fake_pred :
+            the critic's scores of the fake images
+
+        Returns
+        -------
+            gen_loss: a scalar loss value for the current batch of the generator
+        """
+        gen_loss = - torch.mean(crit_fake_pred)
+        return gen_loss
+
     def train(self, train_dataloader, loss="bce", lr=0.0002, k_update_only_disc=1, n_epochs=50, display_step=500, models_dir=settings.MODELS_DIR):
         # TODO: Add save model checkpoints and resume training from checkpoints
         start = time.time()
@@ -98,48 +183,58 @@ class DCGAN:
                 cur_batch_size = len(real)
                 real = real.to(settings.device)
 
-                # train discriminator:
-                self.disc_opt.zero_grad()
-                # generate fake data from latent vectors
-                fake_noise = self.get_noise(cur_batch_size)
-                fake = self.generator(fake_noise)
-                # compute discriminator loss on fake data
-                disc_fake_pred = self.discriminator(fake.detach())
-                disc_fake_loss = self.criterion(disc_fake_pred, torch.zeros_like(disc_fake_pred))
-                # compute discriminator loss on real data
-                disc_real_pred = self.discriminator(real)
-                disc_real_loss = self.criterion(disc_real_pred, torch.ones_like(disc_real_pred))
-                # compute the global discriminator loss as the mean between fake and real batches losses
-                disc_loss = (disc_fake_loss + disc_real_loss) / 2
-                # update discriminator gradients
-                disc_loss.backward(retain_graph=True)
-                # update discriminator optimizer
-                self.disc_opt.step()
-                # keep track of the discriminator loss
-                d_loss += disc_loss.item()  # / display_step
-
-                # every k steps, update generator model
-                if cur_step % k_update_only_disc == 0:
-                    # train generator:
-                    self.gen_opt.zero_grad()
+                mean_disc_loss = 0
+                # train discriminator for k steps:
+                for _ in range(k_update_only_disc):
+                    self.disc_opt.zero_grad()
                     # generate fake data from latent vectors
-                    fake_noise_2 = self.get_noise(cur_batch_size)
-                    fake_2 = self.generator(fake_noise_2)
-                    # compute the generator loss on fake data
-                    disc_fake_pred_2 = self.discriminator(fake_2)
+                    fake_noise = self.get_noise(cur_batch_size)
+                    fake = self.generator(fake_noise)
+                    # compute discriminator loss on fake and real data
+                    disc_fake_pred = self.discriminator(fake.detach())
+                    disc_real_pred = self.discriminator(real)
+                    if loss == "w-gp":
+                        epsilon = torch.rand(len(real), 1, 1, 1, device=settings.device, requires_grad=True)
+                        gradient = self._get_gradient(real, fake.detach(), epsilon)
+                        gp = self._gradient_penalty(gradient)
+                        c_lambda = 10
+                        disc_loss = self.get_crit_loss(disc_fake_pred, disc_real_pred, gp, c_lambda)
+                    else:
+                        disc_fake_loss = self.criterion(disc_fake_pred, torch.zeros_like(disc_fake_pred))
+                        disc_real_loss = self.criterion(disc_real_pred, torch.ones_like(disc_real_pred))
+                        # compute the global discriminator loss as the mean between fake and real batches losses
+                        disc_loss = (disc_fake_loss + disc_real_loss) / 2
+                    mean_disc_loss += disc_loss.item() / k_update_only_disc
+                    # update discriminator gradients
+                    disc_loss.backward(retain_graph=True)
+                    # update discriminator optimizer
+                    self.disc_opt.step()
+                    # keep track of the discriminator loss
+                d_loss += mean_disc_loss
+
+                # train generator:
+                self.gen_opt.zero_grad()
+                # generate fake data from latent vectors
+                fake_noise_2 = self.get_noise(cur_batch_size)
+                fake_2 = self.generator(fake_noise_2)
+                # compute the generator loss on fake data
+                disc_fake_pred_2 = self.discriminator(fake_2)
+                if loss == "w-gp":
+                    gen_loss = self.get_gen_loss(disc_fake_pred_2)
+                else:
                     gen_loss = self.criterion(disc_fake_pred_2, torch.ones_like(disc_fake_pred_2))
-                    # update generator gradients
-                    gen_loss.backward()
-                    # update generator optimizer
-                    self.gen_opt.step()
-                    # keep track of the average generator loss
-                    g_loss += gen_loss.item()  # / display_step
+                # update generator gradients
+                gen_loss.backward()
+                # update generator optimizer
+                self.gen_opt.step()
+                # keep track of the average generator loss
+                g_loss += gen_loss.item()
 
                 # plot training stats
                 if it % 100 == 0 and it > 0:
                     print(
                         f"\nEpoch: [{epoch}/{n_epochs}] \tStep: [{cur_step}/{len(train_dataloader)}]"
-                        f"\tTime: {time.time() - start} (s)\tG_loss: {gen_loss.item()}\tD_loss: {disc_loss.item()}"
+                        f"\tTime: {time.time() - start} (s)\tG_loss: {gen_loss.item()}\tD_loss: {mean_disc_loss.item()}"
                         f"\tD(x): {disc_real_pred.mean().item()}"
                         f"\tD(G(z)): {disc_fake_pred.mean().item()} / {disc_fake_pred_2.mean().item()}")
                 cur_step += 1
