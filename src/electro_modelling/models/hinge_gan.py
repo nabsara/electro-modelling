177a0e9ac4497f9438b9dbf77c967a821ad49ce5
Mathilde Abrassart <abrassart.mathilde@gmail.com>
2021-12-21 13:00:35 +0100

    updated hinge gan

--- src/electro_modelling/models/hinge_gan.py
+++ src/electro_modelling/models/hinge_gan.py
@@ -1,3 +1,6 @@
+import torch
+import torch.nn as nn
+
 from electro_modelling.models.dcgan import DCGAN
 
 
@@ -10,14 +13,24 @@ class HingeGAN(DCGAN):
             init_weights=True
         )
 
-    def _init_optimizer(self, learning_rate):
-        pass
+    def _init_optimizer(self, learning_rate, beta_1 = 0.5, beta_2 = 0.999):
+        self.gen_opt = torch.optim.Adam(self.generator.parameters(),
+lr=learning_rate, betas=(beta_1, beta_2))
+        self.disc_opt = torch.optim.Adam(self.discriminator.parameters(),
+lr=learning_rate, betas=(beta_1, beta_2))
 
     def _init_criterion(self):
-        pass
+        self.criterion = nn.HingeEmbeddingLoss()
 
     def _compute_disc_loss(self, real, fake, disc_real_pred, disc_fake_pred):
-        pass
+        disc_fake_loss = self.criterion(disc_fake_pred,
+torch.zeros_like(disc_fake_pred))
+        disc_real_loss = self.criterion(disc_real_pred,
+torch.ones_like(disc_real_pred))
+        disc_loss = (disc_fake_loss + disc_real_loss) /2
+        return disc_loss
 
     def _compute_gen_loss(self, disc_fake_pred):
-        pass
+        gen_loss = self.criterion(disc_fake_pred,
+torch.ones_like(disc_fake_pred))
+        return gen_loss
