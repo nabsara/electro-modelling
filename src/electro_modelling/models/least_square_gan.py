632160583cf1684ac40fded1dc7e49b1bac0bf47
JDEXGITHUB <jeanda98@hotmail.fr>
2021-12-28 14:50:37 +0000

    add least square dcgan, with comments

--- src/electro_modelling/models/least_square_gan.py
+++ src/electro_modelling/models/least_square_gan.py
@@ -17,16 +17,46 @@ class LeastSquareGAN(DCGAN):
         self.gen_opt = torch.optim.Adam(self.generator.parameters(), lr=learning_rate, betas=(beta_1, beta_2))
         self.disc_opt = torch.optim.Adam(self.discriminator.parameters(), lr=learning_rate, betas=(beta_1, beta_2))
 
-#     def _init_criterion(self):
-#         self.criterion = nn.BCELoss()
+    def _init_criterion(self):
+        self.criterion = nn.MSELoss()
 
     def _compute_disc_loss(self, real, fake, disc_real_pred, disc_fake_pred):
-        disc_fake_loss = 0.5 * torch.mean((disc_fake_pred+torch.ones_like(disc_fake_pred))**2)
-        disc_real_loss = 0.5 * torch.mean((disc_real_pred-torch.zeros_like(disc_real_pred))**2)
+        """
+        Return the loss of a critic given the critic's scores for fake and real images.
+
+        Parameters
+        ----------
+        real :
+            current batch of real images
+        fake :
+            current batch of fake images
+        disc_real_pred :
+            the critic's scores of the real images
+        disc_fake_pred :
+            the critic's scores of the fake images
+
+        Returns
+        -------
+            a scalar for the critic's loss for the current batch
+        """
+        disc_fake_loss = self.criterion(disc_fake_pred, torch.zeros_like(disc_fake_pred))
+        disc_real_loss = self.criterion(disc_real_pred, torch.ones_like(disc_real_pred))
         disc_loss = (disc_fake_loss + disc_real_loss) /2
         return disc_loss
 
 
     def _compute_gen_loss(self, disc_fake_pred):
-        gen_loss = 0.5 * torch.mean((disc_fake_pred+torch.ones_like(disc_fake_pred))**2)
+        """
+        Return the loss of a generator given the critic's scores of the generator's fake images.
+
+        Parameters
+        ----------
+        disc_fake_pred :
+            the critic's scores of the fake images
+
+        Returns
+        -------
+            a scalar for the generator loss for the current batch
+        """
+        gen_loss = self.criterion(disc_fake_pred, torch.ones_like(disc_fake_pred))
         return gen_loss
